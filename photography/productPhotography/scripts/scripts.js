9c1b3a87036e4717f21ed21232515fbaa13d408a
Jacob MacDonald <46459062+MacDonald-Jacob@users.noreply.github.com>
2019-07-23 11:44:32 -0600

    manipulated css with javascript on product page

---
+++ photography/productPhotography/scripts/scripts.js
@@ -0,0 +1,40 @@
+//change text with mouseover and mouseout
+function mOver(obj) {
+    obj.innerHTML = "www.blacklistedbanditsandco.com"
+    obj.style.width = "100%";
+    obj.style.height = "50px";
+    obj.style.color = "black";
+    obj.style.fontSize = "16px";
+    obj.style.marginTop = "20px";
+
+}
+function mOut(obj) {
+    obj.innerHTML = "Blacklisted Bandits"
+    obj.style.width = "100%";
+    obj.style.height = "70px";
+    obj.style.color = "black";
+    obj.style.fontSize = "50px";
+    obj.style.marginTop = "20px";
+    obj.style.marginBottom = "20px";
+    obj.style.fontFamily = "Black Ops One, cursive";
+
+}
+
+function mOver2(obj) {
+    obj.innerHTML = "www.wildragboutique.com"
+    obj.style.width = "100%";
+    obj.style.height = "50px";
+    obj.style.color = "black";
+    obj.style.fontSize = "18px";
+}
+function mOut2(obj) {
+    obj.innerHTML = "Wild Rag Boutique"
+    obj.style.width = "100%";
+    obj.style.height = "70px";
+    obj.style.color = "black";
+    obj.style.fontSize = "50px";
+    obj.style.marginTop = "50px";
+    obj.style.marginBottom = "5rem";
+    obj.style.fontFamily = "Ewert,cursive";
+
+}
