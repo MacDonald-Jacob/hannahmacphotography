c305276f2399594236a54c1deb3da78a0d09100a
Austin Kincade <austinkincade@Austins-MacBook-Pro.local>
2019-07-24 18:31:27 -0600

    Editing the dom stuff

--- scripts/DOMmanipulation.js
+++ scripts/DOMmanipulation.js
@@ -1,34 +1,22 @@
 var addButton = document.getElementById('addButton');
 var itemList = document.getElementById('interests');
 
-// Form submit event
-form.addEventListener('submit', addItem);
+// Add item button click
+addButton.addEventListener('click', addItem);
 
 // Add item
 function addItem(e) {
-   e.preventDefault();
-
    // Get input value
    var newItem = document.getElementById('item').value;
 
-   // Create new li element
-   var li = document.createElement('li');
-   // Add class
-   li.className = 'list-group-item';
+   // Create new option element
+   var li = document.createElement('option');
+
    // Add text node with input value
    li.appendChild(document.createTextNode(newItem));
 
-   // Create del button element
-   var deleteBtn = document.createElement('button');
-
-   // Add classes to del button
-   deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-
    // Append text node
    deleteBtn.appendChild(document.createTextNode('X'));
-
-   // Append button to li
-   li.appendChild(deleteBtn);
 
    // Append li to list
    itemList.appendChild(li);
